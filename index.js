8fb57f72e46f0c162871ec1143f8ffb21a4a2f8d
Saad Usmani <saadausmani123@gmail.com>
2025-02-12 15:22:20 -0800

    Added all nav skips + smooth scrolling

--- index.js
+++ index.js
@@ -158,6 +158,13 @@ document.querySelectorAll('.text-5xl > span').forEach(letter => {
     })
 })
 
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", function(e){              // to prevent the default skip event
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({behavior: 'smooth'})
+    })
+})
+
 // document.querySelector("#audio").addEventListener("click", function(){
     
 // })
